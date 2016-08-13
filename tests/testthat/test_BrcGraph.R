8f7b57d1d2e01552a889f8f5413e38694ee39b21
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-08-12 22:32:14 -0400

    Added more tests for BrcGraph

--- tests/testthat/test_BrcGraph.R
+++ tests/testthat/test_BrcGraph.R
@@ -1,22 +1,37 @@
 context("Test BrcGraph")
 
-test_that("BrcGraph errors when graph is not igraph",{
+test_that("it errors when graph is not igraph",{
+  set.seed(10)
   mat <- matrix(rbinom(100,1,0.5),10,10)
   expect_error(BrcGraph(mat, "asdf", 5))
 })
 
-test_that("BrcGraph errors when type is not character", {
+test_that("it errors when type is not character", {
   graph <- igraph::make_graph(c(1, 2, 2, 3, 3, 4, 5, 6), directed = FALSE)
   expect_error(BrcGraph(graph, 5, 5))
 })
 
-test_that("BrcGraph errors when parameter is not numeric", {
+test_that("it errors when parameter is not numeric", {
   graph <- igraph::make_graph(c(1, 2, 2, 3, 3, 4, 5, 6), directed = FALSE)
   expect_error(BrcGraph(graph, "asdf", "asdf"))
 })
 
-test_that("isValid works", {
+test_that("it can validate itself", {
   graph <- igraph::make_graph(c(1, 2, 2, 3, 3, 4, 5, 6), directed = FALSE)
   obj <- BrcGraph(graph, "asdf", 5)
+  expect_true(brcbase::isValid(obj))
+})
+
+test_that("it makes a valid BrcGraph when making a graph from matrix", {
+  set.seed(10)
+  mat <- matrix(rbinom(100,1,0.5),10,10)
+  obj <- BrcGraph(igraph::graph_from_incidence_matrix(mat), "asdf", 5)
+  expect_true(brcbase::isValid(obj))
+})
+
+test_that("it makes a valid BrcGraph when making a graph from edge matrix", {
+  set.seed(10)
+  mat <- matrix(c(1:10,2:11), ncol = 2, nrow = 10)
+  obj <- BrcGraph(igraph::graph_from_edgelist(mat), "asdf", 5)
   expect_true(brcbase::isValid(obj))
 })
