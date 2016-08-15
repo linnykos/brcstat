b4727b4239b86c3ccc56d7aaaeb4c74f1d9ab66c
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-08-15 15:55:03 -0400

    Changing the isValid functions for BrcFmriGraphList

--- R/BrcFmriGraphList.R
+++ R/BrcFmriGraphList.R
@@ -1,4 +1,9 @@
 BrcFmriGraphList <- function(graph.list, id, parcellation){
+  if(!is.list(graph.list)) stop("graph.list must be a list")
+  if(!is.character(id)) stop("id must be a character")
+  if(class(parcellation) != "BrcParcellation")
+    stop("obj$parcellation must be of class BrcParcellation")
+  
   obj <- structure(list(graph.list = graph.list, id = id, 
     parcellation = parcellation), class = "BrcFmriGraphList")
   
@@ -8,15 +13,8 @@ BrcFmriGraphList <- function(graph.list, id, parcellation){
 }
 
 isValid.BrcFmriGraphList <- function(obj){
-  if(class(obj) != "BrcFmriGraphList") stop("obj must be of class 
-    BrcFmriGraphList")
-  
-  if(!is.list(obj$graph.list)) stop("obj$graph.list must be a list")
   if(!all(sapply(obj$graph.list, class) == "BrcGraph")) stop("obj$graph.list must
      be a list of BrcGraph")
-  if(!is.character(obj$id)) stop("obj$id must be character")
-  if(class(obj$parcellation) != "BrcParcellation") stop("obj$parcellation 
-     must be of class BrcParcellation")
   if(!brcbase::isValid(obj$parcellation)) stop("obj$parcellation must be valid")
 
   .checkNumberofNodes(obj)
