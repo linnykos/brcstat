208e72a2c8e5c67401d6c7f18f9a018eb5511c0d
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-08-16 00:29:20 -0400

    put the parcellation plot code from brcvis into brcstat

--- R/plot.BrcFmriGraphList.R
+++ R/plot.BrcFmriGraphList.R
@@ -1,3 +1,53 @@
-plot.BrcFmriGraphList <- function(x, y = NA, ...){
+plot.BrcFmriGraphList <- function(x, y = NA, graph.idx, ...){
+  if(!isValid(x)) stop("x must be of class BrcFmriGraphList")
+  if(!is.numeric(graph.idx)) stop("idx must be a numeric")
+  if(graph.idx < 0 | graph.idx%%1 != 0) stop("idx must be a positive integer")
+  if(graph.idx > length(x$graph.list)) stop("idx must be less than the number of
+    graphs in x$graph.list")
   
+  
+}
+
+.parcellationToArray <- function(parcellation) {
+  data <- parcellation$partition
+  array(data=data, dim=parcellation$dim3d)
+}
+
+.makeIndexSequence <- function(max, length) {
+  round(seq(1, max, length.out=length))
+}
+
+.extractSlices <- function(arr, indices, dim) {
+  .splitAlongDim(arr, dim)[indices]
+}
+
+
+.plotSlices <- function(slices, numParcels, colors) {
+  layout <- .plotLayout(numSlices=length(slices))
+  maxParcel <- max(unlist(slices))
+  graphics::par(mfrow=c(layout$nrow, layout$ncol), mar=rep(0.2, 4), bg="black")
+  for (i in 1:length(slices)) {
+    graphics::image(slices[[i]],
+                    asp=ncol(slices[[i]]) / nrow(slices[[i]]),
+                    breaks=(0:(maxParcel + 1)) - 0.5,
+                    bty="n",
+                    col=colors,
+                    xaxt="n",
+                    yaxt="n")
+  }
+}
+
+.plotLayout <- function(numSlices) {
+	nrow = ceiling(sqrt(numSlices / 2))
+	ncol = ceiling(numSlices / nrow)
+	list(nrow=nrow, ncol=ncol)
+}
+
+# This function borrowed from
+# http://stackoverflow.com/questions/20198751/three-dimensional-array-to-list
+# Thanks, internet!
+.splitAlongDim <- function(arr, dim) {
+  stats::setNames(lapply(split(arr, arrayInd(seq_along(arr), dim(arr))[, dim]),
+                         array, dim=dim(arr)[-dim], dimnames(arr)[-dim]),
+                  dimnames(arr)[[dim]])
 }
---
+++ tests/testthat/test_plot.BrcFmriGraphList.R
@@ -0,0 +1,77 @@
+context("Test plot.BrcFmriGraphList")
+
+
+# Test .parcellationToArray()
+
+dim3d <- c(2, 2, 2)
+partition <- c(0, 0, 1, 1, 2, 2, 3, 3)
+parcel <- brcbase::BrcParcellation(dim3d=dim3d, partition=partition)
+
+test_that("the dims of the array should be the dims of the parcellation", {
+  arr <- .parcellationToArray(parcel)
+  expect_equal(dim(arr), c(2, 2, 2))
+})
+
+
+# test .makeIndexSequence()
+
+test_that("it should return a sequence of the user-specified length", {
+  seq <- .makeIndexSequence(max=15, length=13)
+  expect_equal(length(seq), 13)
+})
+
+test_that("it should return a vector of integers", {
+  seq <- .makeIndexSequence(max=15, length=13)
+  expect_equal(seq, as.integer(seq))
+})
+
+test_that("all of the values should be positive", {
+  seq <- .makeIndexSequence(max=15, length=13)
+  expect_true(all(seq > 0))
+})
+
+# test .extractSlices()
+arr <- array(c(rep.int(0, 16),
+               0, 0, 0, 0, 0, 1, 2, 0, 0, 3, 4, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 5, 6, 0, 0, 7, 8, 0, 0, 0, 0, 0,
+               rep.int(0, 16)),
+               dim=c(4, 4, 4))
+
+test_that("it should return a list of the same length as the indices list", {
+  slices <- .extractSlices(arr=arr, indices=c(2, 3), dim=1)
+  expect_equal(length(slices), 2)
+})
+
+test_that("it should contain elements whose dimensions are one less than arr", {
+  slices <- .extractSlices(arr=arr, indices=c(2, 3), dim=1)
+  dimensionLengths <- lapply(slices, function(x) length(dim(x)))
+  expect_true(all(dimensionLengths == length(dim(arr)) - 1))
+})
+
+test_that("it contains the slices corresponding to the indices", {
+  slices <- .extractSlices(arr=arr, indices=c(2, 3), dim=2)
+  expect_equal(slices, .splitAlongDim(arr, 2)[2:3])
+})
+
+# test .plotLayout()
+
+test_that("the product of nrow and ncol is larger than numSlices", {
+  layout <- .plotLayout(10)
+  expect_true(layout$nrow * layout$ncol >= 10)
+})
+
+# test splitAlongDim()
+
+arr <- array(c(1, 2, 3, 4, 5, 6, 7, 8), dim=c(2, 2, 2))
+
+test_that("it splits an array along a dimension", {
+  expected <- list(array(c(1, 2, 3, 4), dim=c(2, 2)),
+                   array(c(5, 6, 7, 8), dim=c(2, 2)))
+  expect_equal(.splitAlongDim(arr, 3), expected)
+})
+
+test_that("you can choose the dimension along which to split", {
+  expected <- list(array(c(1, 2, 5, 6), dim=c(2, 2)),
+                   array(c(3, 4, 7, 8), dim=c(2, 2)))
+  expect_equal(.splitAlongDim(arr, 2), expected)
+})
